679843dd3479cb21d7f0e298584e2206893296f7
gary-yaral <79531864+Gary-Yaral@users.noreply.github.com>
2022-01-06 17:49:02 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { taskReducer } from '/src/reducers/reducer.js'
-import{ newTask, removeTask, strokeTask, updateTask } from '/src/actions/actions.js'
+import { taskReducer } from './src/reducers/reducer.js'
+import{ newTask, removeTask, strokeTask, updateTask } from './src/actions/actions.js'
 
 let data= document.querySelector('.tasks__data')
 let addBtn= document.querySelector('.add')
